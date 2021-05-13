152d4841f923816fd1ce3ad46b18b1f3ab19a2ab
Princess <adwoaasante88@gmail.com>
2021-05-13 20:29:20 +0000

    more days

--- day-28/main.py
+++ day-28/main.py
@@ -9,14 +9,43 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    title.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmark["text"] = ""
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title.config(text="Long Break", fg="Red")
+
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title.config(text="Short Break", fg="Pink")
+
+    else:
+        count_down(work_sec)
+        title.config(text="Work")
+
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -29,8 +58,16 @@ def count_down(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
-
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        mark = ""
+        start_timer()
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+            checkmark["text"] = mark
+            print(reps)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -39,20 +76,19 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
-
-timer = Label(fg=GREEN, text="Timer", font=(FONT_NAME, 25,"bold"), bg=YELLOW)
-timer.grid(column=1, row=0)
+title = Label(fg=GREEN, text="Timer", font=(FONT_NAME, 25,"bold"), bg=YELLOW)
+title.grid(column=1, row=0)
 
 start = Button(text="Start", bg=GREEN, fg="white", highlightthickness=0, command=start_timer)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", bg=GREEN, fg="white")
+reset = Button(text="Reset", bg=GREEN, fg="white", command=reset_timer)
 reset.grid(column=2, row=2)
 
-checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark = Label(text="", fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=2)
 
-canvas = Canvas(width=200, height= 224, highlightthickness=0, bg=YELLOW)
+canvas = Canvas(width=200, height=224, highlightthickness=0, bg=YELLOW)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 20, "bold"))
